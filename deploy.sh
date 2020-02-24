04aac872e406bd54babff4f128f0a38d2eab3ef1
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-24 11:56:07 +0300

    Deploy: amend not working, revert

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,6 @@ echo "export const version = '${VERSION}';" > src/config.js
 echo "export const released = '$(date --iso-8601=seconds)';" >> src/config.js
 echo "export const releaseLink = 'https://github.com/burkov/lfs-errors-dashboard/releases/tag/${VERSION}';" >> src/config.js
 git add .
-git ci --amend --no-edit
-#npm run deploy
+git ci -m "Version ${VERSION} pushed to app config"
+npm run deploy
 
