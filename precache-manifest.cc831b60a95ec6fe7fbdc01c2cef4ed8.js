bd868d04809b855e82bc4efd1fb7768f494d8750
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-24 12:26:20 +0300

    Updates

--- precache-manifest.cc831b60a95ec6fe7fbdc01c2cef4ed8.js
+++ precache-manifest.cc831b60a95ec6fe7fbdc01c2cef4ed8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a5dd1235d37053d1773a391be01c948e",
+    "revision": "ec6fce79d3224ed3604de64e150543e6",
     "url": "/lfs-errors-dashboard/index.html"
   },
   {
-    "revision": "59a0f02048c6208427e7",
+    "revision": "1502067940bfac469502",
     "url": "/lfs-errors-dashboard/static/css/2.5bc4965f.chunk.css"
   },
   {
-    "revision": "cb2a0ffc24e708e80bc3",
+    "revision": "4c48ec7b5f0ef4091abb",
     "url": "/lfs-errors-dashboard/static/css/main.5bc90a2e.chunk.css"
   },
   {
-    "revision": "59a0f02048c6208427e7",
-    "url": "/lfs-errors-dashboard/static/js/2.a56f8c58.chunk.js"
+    "revision": "1502067940bfac469502",
+    "url": "/lfs-errors-dashboard/static/js/2.f6cedf9b.chunk.js"
   },
   {
     "revision": "46f63f8626516ab37e48a023f0c6fcd4",
-    "url": "/lfs-errors-dashboard/static/js/2.a56f8c58.chunk.js.LICENSE.txt"
+    "url": "/lfs-errors-dashboard/static/js/2.f6cedf9b.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "cb2a0ffc24e708e80bc3",
-    "url": "/lfs-errors-dashboard/static/js/main.25039340.chunk.js"
+    "revision": "4c48ec7b5f0ef4091abb",
+    "url": "/lfs-errors-dashboard/static/js/main.2558a2fc.chunk.js"
   },
   {
     "revision": "e0c257df9b69dfa8b9a5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/lfs-errors-dashboard/precache-manifest.946914019b826e09bba283c9e22a0222.js"
+  "/lfs-errors-dashboard/precache-manifest.cc831b60a95ec6fe7fbdc01c2cef4ed8.js"
 );
 
 self.addEventListener('message', (event) => {
