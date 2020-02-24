135f0e7bd3455b2541952dcd5312a8845a4fd643
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-24 11:31:17 +0300

    Version v0.1.1 pushed to app config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const version = 'v0.1.1';
-export const released = 'Mon 24 Feb 11:31:10 MSK 2020';
+export const released = 'Mon 24 Feb 11:31:17 MSK 2020';
 export const releaseLink = 'https://github.com/burkov/lfs-errors-dashboard/releases/tag/v0.1.1';
