992d78fcaf6494ca4999d1c552715cc800f76293
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-24 11:31:10 +0300

    Version v0.1.1 pushed to app config

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 VERSION=$(git describe --tags --abbrev=0)
 echo "export const version = '${VERSION}';" > src/config.js
 echo "export const released = '$(date)';" >> src/config.js
-echo "export const releaseLink = 'https://github.com/burkov/lfs-errors-dashboard/releases/tag/${VERSION}';"
+echo "export const releaseLink = 'https://github.com/burkov/lfs-errors-dashboard/releases/tag/${VERSION}';" >> src/config.js
 git add .
 git ci -m "Version ${VERSION} pushed to app config"
 
