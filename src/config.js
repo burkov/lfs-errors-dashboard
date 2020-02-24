44666ce0588e0daff769c97ec5855e8963670d76
alexander.burkov <alexander.burkov@jetbrains.com>
2020-02-24 11:54:02 +0300

    Deploy: amend last commit instead of creating new one

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const version = 'v0.1.2';
-export const released = '2020-02-24T11:38:46+03:00';
+export const released = '2020-02-24T11:54:28+03:00';
 export const releaseLink = 'https://github.com/burkov/lfs-errors-dashboard/releases/tag/v0.1.2';
